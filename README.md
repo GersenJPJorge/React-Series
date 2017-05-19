3ebd5d2f3d63035fdeef26935280276c85220c6e
GersenJPJorge <gersen@ibest.com.br>
2017-05-19 18:37:04 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# React-Series
-React Series
+# React Series
+Este pacote contém os arquivos básicos para iniciar as vídeo aulas
+de React do [Canal V++](https://youtube.com/user/VPlusPlus).
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import ReactDom from 'react-dom';
+
+import App from '.src/components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("app")
+);
